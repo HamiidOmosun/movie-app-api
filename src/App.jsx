9a43ef8365b5a32e9ead2efc8c661fd64eb67210
HamiidOmosun <Abdulhamiidomosun@gmail.com>
2025-12-17 12:49:30 +0100

    working on the fetch logic

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,34 @@ import React, {useEffect, useState} from "react";
 import HomePage from "./assets/Pages/HomePage.jsx";
 import NavBar from "./assets/components/NavBar.jsx";
 
-const API_BASE_URL = 'https://api.themoviedb.org/3/';
+const API_BASE_URL = 'https://api.themoviedb.org/3';
 
-const API_KEY = import.meta.env.VITE_TMBD_API_KEY;
 
 const API_OPTIONS = {
     method: "GET",
     headers: {
         accept: "application/json",
-        Authorization: `Bearer ${API_KEY}`
+        Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`
     }
 }
 
 const App = () => {
     const [searchTerm, setSearchTerm] = useState('');
 
+    const [error, setError] = useState('');
+
+    const [movieList, setMovieList] = useState([]);
+
     const fetchMovies = async () => {
-        try{
+        try{ const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+
+            const res = await fetch(endpoint, API_OPTIONS);
+
+            if(!res.ok){
+                throw new Error('failed to fetch movies');
+            }
+
+            const data = await res.json();
 
         }catch (error){
             console.error(`Error fetching movies: ${error}`);
@@ -28,7 +39,7 @@ const App = () => {
     }
 
     useEffect(() => {
-
+        fetchMovies();
     }, [])
     //states are
 
